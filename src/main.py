82795f3f5a5a624663767665f4ac063886b96baa
Parsa Toopchinezhad <87566207+Procedurally-Generated-Human@users.noreply.github.com>
2023-08-01 17:36:58 +0330

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,12 +1,32 @@
 import gymnasium as gym
-from gym_recorder import Recorder
+from stable_baselines3 import PPO
+from sb3_contrib import TQC
+import os 
 
 
-env = gym.make("BipedalWalker-v3", render_mode="human")
 
-
-
+env = gym.make("BipedalWalker-v3")
 env.reset()
-for i in range(100):
-    action = env.action_space.sample()
-    env.step(action)
+
+
+
+
+model = TQC("MlpPolicy", env, verbose=1, tensorboard_log="./ppo_bipedal_tensorboard/")
+model.learn(total_timesteps=100000)
+model.save("./saveppo")
+
+
+# load model
+# model = PPO.load("./saveppo.zip", env)
+
+# render agent
+env = gym.make("BipedalWalker-v3", render_mode="human")
+for i in range(10):
+    state, _ = env.reset()
+    while True:
+        env.render()
+        action, _ = model.predict(state)
+        state, reward, done, _, _ = env.step(action)
+        if done:
+            break
+env.close()
